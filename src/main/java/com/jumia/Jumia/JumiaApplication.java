24f557f848f4848207accb4c4475b57be585f112
mohamed14787 <mohamedelshaarawy87@gmail.com>
2024-10-11 19:51:00 +0200

    first commit

---
+++ src/main/java/com/jumia/Jumia/JumiaApplication.java
@@ -0,0 +1,13 @@
+package com.jumia.Jumia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JumiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JumiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jumia/Jumia/JumiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jumia.Jumia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JumiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
